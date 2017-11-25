b574056f3e8d07214411d58663348f921ed8f4bc
moni <monika.rusajczyk@gmail.com>
2017-11-25 22:50:01 +0100

    object -> dotNotation VS BracketNotation

---
+++ 2_DotVsBracket.js
@@ -0,0 +1,24 @@
+
+var aFalse = 'aTrue';
+
+var a = {
+    aTrue: 'True',
+    aFalse: 'False'
+};
+
+var x = a.aTrue;
+console.log(x);     // -> True
+
+var y = a.aFalse;
+console.log(y);     // -> False
+
+
+/* let the black magic happen! */
+
+
+var q = a[aFalse];  // ! invokes a variable declared outside of the object
+console.log(q);     // -> True
+
+var z = a.aFalse;   // ! invokes a variable declared inside of the object
+console.log(z);     // -> False
+
