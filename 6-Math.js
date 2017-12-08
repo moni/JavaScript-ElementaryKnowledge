641d8bfd6e37dac9021a35903c116c04a5c083c0
moni <monika.rusajczyk@gmail.com>
2017-12-08 18:00:38 +0100

    Math - operators

---
+++ 6-Math.js
@@ -0,0 +1,11 @@
+var firstNumber = 16;
+var secondNumber = 12;
+var thirdNumber = 0.12324234;
+
+console.log(firstNumber + secondNumber);     // -> 28
+
+console.log(firstNumber - secondNumber);    // -> 4
+
+console.log(firstNumber * secondNumber);    // -> 192
+
+console.log(firstNumber / secondNumber);    // -> 1.3333333333333333
